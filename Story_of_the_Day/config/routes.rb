9b8bc4a867a0c33308e66ffb1f746ed1626a96f9
esau jimenez <ovan20081@outlook.com>
2017-02-27 17:23:19 -0800

    update

---
+++ Story_of_the_Day/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  root to: 'users#new'
+
+  get 'users/new' => 'users#new', as: :new_user
+  post 'users' => 'users#create'
+
+  get '/login'     => 'sessions#new'
+ post '/login'    => 'sessions#create'
+ delete '/logout' => 'sessions#destroy'
+
+ get "posts/" => "posts#index"
+get "posts/new" => "posts#new", as: :new_post
+post "posts/" => "posts#create"
+get "posts/:id/edit" => "posts#edit", as: :edit_post
+patch "posts/:id" => "posts#update"
+get "posts/:id" => "posts#show", as: :post
+delete "posts/:id" => "posts#destroy"
+
+end
